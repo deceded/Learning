4bc37e5123efa2754d1687a0542dd7bc36b6da10
deceded <31942630+deceded@users.noreply.github.com>
2021-09-09 19:45:08 -0400

    c6v13

--- guessinggame.py
+++ guessinggame.py
@@ -1,27 +1,39 @@
 import random
 
-print("-----------------------")
-print("    Guessing Game!")
-print("-----------------------")
 
-ammo_count = random.randint(1, 100)
-attempt_limit = 5
-attempts = 0
+def main():
+    show_header()
 
-print("Guess the number of bullets and get a free mag!")
-print()
+def show_header():
+    print("-----------------------")
+    print("    Guessing Game!")
+    print("-----------------------")
 
-while attempts < attempt_limit:
-    guess_text = input("How many bullets are in the box? ")
-    guess = int(guess_text)
-    attempts += 1
 
-    if guess == ammo_count:
-        print(f"Correct! It was {guess}")
-        exit()
-    elif guess < ammo_count:
-        print("Sorry, that's too low!")
-    else:
-        print("Sorry, that's too high!")
+def data_get():
+    ammo_count = random.randint(1, 100)
+    attempt_limit = 5
+    attempts = 0
+    print("Guess the number of bullets and get a free mag!")
+    print()
 
-print(f"Nope! The answer was {ammo_count}")
+
+def input_get(attempts, attempt_limit, ammo_count):
+    while attempts < attempt_limit:
+        guess_text = input("How many bullets are in the box? ")
+        guess = int(guess_text)
+        attempts += 1
+
+        if guess == ammo_count:
+            print(f"Correct! It was {guess}")
+            exit()
+        elif guess < ammo_count:
+            print("Sorry, that's too low!")
+        else:
+            print("Sorry, that's too high!")
+
+    print(f"Nope! The answer was {ammo_count}")
+
+
+if __name__ == '__main__':
+    main()
